aa4cd98bb2a64ea16e945f132c0d058d582ec87f
Matt Berther <matt@berther.io>
2019-01-30 23:29:09 -0700

    initial commit

---
+++ zsh-pyenv.plugin.zsh
@@ -0,0 +1,30 @@
+GITHUB="https://github.com"
+
+[[ -z "$PYENV_HOME" ]] && export PYENV_HOME="$HOME/.pyenv"
+
+_zsh_pyenv_install() {
+    echo "Installing pyenv..."
+    git clone "${GITHUB}/pyenv/pyenv.git"            "${PYENV_HOME}"
+    git clone "${GITHUB}/pyenv/pyenv-doctor.git"     "${PYENV_HOME}/plugins/pyenv-doctor"
+    git clone "${GITHUB}/pyenv/pyenv-installer.git"  "${PYENV_HOME}/plugins/pyenv-installer"
+    git clone "${GITHUB}/pyenv/pyenv-update.git"     "${PYENV_HOME}/plugins/pyenv-update"
+    git clone "${GITHUB}/pyenv/pyenv-virtualenv.git" "${PYENV_HOME}/plugins/pyenv-virtualenv"
+    git clone "${GITHUB}/pyenv/pyenv-which-ext.git"  "${PYENV_HOME}/plugins/pyenv-which-ext"
+}
+
+_zsh_pyenv_load() {
+    # export PATH
+    export PATH="$PYENV_HOME/bin:$PATH"
+
+    eval "$(pyenv init -)"
+    eval "$(pyenv virtualenv-init -)"
+}
+
+# install pyenv if it isnt already installed
+[[ ! -f "$PYENV_HOME/libexec/pyenv" ]] && _zsh_pyenv_install
+
+# load pyenv if it is installed
+if [[ -f "$PYENV_HOME/libexec/pyenv" ]]; then
+    _zsh_pyenv_load
+fi
+
